9cd83e2db38d9af335c3035e0aaba2c562eb5758
Jean Hsieh <hsiehzq@gmail.com>
2024-08-29 22:54:08 +0900

    0829commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
-import Header from './Header'
 import Description from './Description'
+import DogImage from './DogImage'
+import { useState } from 'react'
+import Header from './Header'
 
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/affenpinscher/n02110627_8099.jpg")
+
+  const fetchRandomDogImage = async () => {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random")
+      const data = await response.json()
+      setDogUrl(data.message)
+    } catch (error) {
+      console.error("Error fetching dog image:", error)
+    }
+  }
+
   return (
     <>
       <Header />
-      <Description />
+      <Description fetchRandomDogImage={fetchRandomDogImage} />
+      <DogImage url={dogUrl} />
     </>
   )
 }
 
-export default App
+export default App;
